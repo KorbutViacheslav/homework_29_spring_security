7dd4e3916ac178a5cea7816a3473befdd2f43085
KorbutViacheslav <watchinggameslive@gmail.com>
2023-04-25 07:55:18 +0300

    add app

---
+++ src/main/java/com/example/homework_29_spring_security/Homework29SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_29_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework29SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework29SpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_29_spring_security/Homework29SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_29_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework29SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
